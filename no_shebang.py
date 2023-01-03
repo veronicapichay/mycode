bbb79ad69591a4d765355026da5869309214dc89
veronica <veronicapichay@gmail.com>
2023-01-03 23:07:42 +0000

    learning about input function

---
+++ no_shebang.py
@@ -0,0 +1,7 @@
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+print("ello!")
+print("Did you say, hello?")
+print("No, I said ello, but that\'s close enough.")
+
