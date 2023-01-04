4fbf511c9dd38b8b2a87c6fa0474d3e561e3e193
veronica <veronicapichay@gmail.com>
2023-01-04 17:06:21 +0000

    challenge18

---
+++ challenge18/cha18.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Veronica Pichay | TLG Learning
+   CHALLENGE 18  - Solution"""    
+
+def main():
+
+    user_name = input("Please enter your name: ")
+
+    day_of_the_week = input("What is today? ")
+    
+    print("Hello, " + user_name + "! Happy " + day_of_the_week)
+
+main()
+
