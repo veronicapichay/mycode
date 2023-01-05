4dbe6d90422be7f3bbdfedb517034d4025746fd0
veronica <veronicapichay@gmail.com>
2023-01-05 16:20:32 +0000

    manipulating lists

---
+++ listchallenge.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+heroes= ["Spiderman", "Batman", "Black Panther", "Wonder Woman", "Storm"]
+
+# PART 1
+# Print out your favorite character from this list! The output would look something like:
+print(f"My favorite character is {heroes[4]}!")
+
+# PART 2
+# Ask the user to pick a number between 1 and 100.
+# Convert the input into an integer.
+
+pick= int(input("Pick a number between 1 and 100\n"))
+nums= [1, -5, 56, 987, 0]
+
+# PART 3
+print(max(nums))
+
