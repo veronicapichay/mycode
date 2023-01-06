5265ec9d0edf2f984c3cf20d6c652e77215b0dd7
veronica <veronicapichay@gmail.com>
2023-01-06 21:43:06 +0000

    studying for logic

---
+++ miniproj/moduleimport.py
@@ -0,0 +1,25 @@
+import html
+
+trivia= {
+         "category": "Entertainment: Film",
+         "type": "multiple",
+         "question": "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+         "correct_answer": "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+         "incorrect_answers": [
+             "&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+             "&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+             "&quot;Round up the usual suspects.&quot;"
+            ]
+        }
+
+question= html.unescape(trivia["question"])
+correct= html.unescaped(trivia["correct_answer"])
+incorrect1= html.unescape(trivia["incorrect_answers"][0])
+incorrect2= trivia["incorrect_answers"][1]
+incorrect3= trivia["incorrect_answers"][2]
+
+print(question)
+
+print(correct)
+
+print(incorrect)
