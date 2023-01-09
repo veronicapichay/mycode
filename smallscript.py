5d58614869b1f8216352640e59114edaff924385
veronica <veronicapichay@gmail.com>
2023-01-09 19:05:18 +0000

    chinese zodiac sign checker

---
+++ smallscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
+
