56ebae9bac136492b0018b5cc27b1def78d3fd0c
veronica <veronicapichay@gmail.com>
2023-01-13 16:03:09 +0000

    Errors redirects and limits

---
+++ flaskapi/chall1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Veronica Pichay | TLG Learning
+   SOLUTION 01 - How quickly can we get to 50 requests (per hour) to be rate limited
+   by FlaskLimiter? Run the script to get results."""
+
+import time
+import requests
+
+URI = "http://localhost:2224/"
+
+def main():
+
+    # get the current time
+    start_time = time.time()
+
+    # start an infinite loop
+    while True:
+        r = requests.get(f"{URI}fast")  # this URI is limited by 50 lookups per hour
+        if r.status_code != 200:
+            end_time = time.time()
+            break # stop looping, as we have hit the limit
+
+    # display the total time it took to perform the lookups
+    print(f"To reach the limit of /fast, it took {end_time - start_time} seconds")
+
+# invoke the main function
+if __name__ == "__main__":
+    main()
+
